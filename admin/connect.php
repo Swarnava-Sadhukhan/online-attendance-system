93ba3acbe986316c995ab1702d02ba657d6d9a75
Swarnava Sadhukhan <sadhu.man11@gmail.com>
2020-11-23 02:46:14 +0530

    remote database

--- admin/connect.php
+++ admin/connect.php
@@ -1,7 +1,24 @@
 <?php
 //establishing connection with database.
+/
+*
+//localhost database
+$host = "localhost";
+$username = "root";
+$pass = "";
+$db="attsystem";
 
-$con=mysqli_connect('localhost','root','') or die('Cannot connect to server');
-mysqli_select_db($con,'attsystem') or die ('Cannot found database');
+*/
+
+
+//remote database
+$host = "remotemysql.com";
+$username = "QFUckEO5eQ";
+$pass = "WwbDq5zKub";
+$db="QFUckEO5eQ";
+
+
+$con=mysqli_connect($host,$username, $pass, $db) or die('Cannot connect to server');
+mysqli_select_db($con,$db) or die ('Cannot found database');
 
 ?>
--- connect.php
+++ connect.php
@@ -1,11 +1,23 @@
 <?php
 
-
+/*
+//localhost database
 $host = "localhost";
 $username = "root";
 $pass = "";
-$con = mysqli_connect($host, $username, $pass, "attsystem");
-mysqli_select_db($con,'attsystem') or die ('Cannot found database');
+$db="attsystem";
+
+*/
+
+//remote database
+$host = "remotemysql.com";
+$username = "QFUckEO5eQ";
+$pass = "WwbDq5zKub";
+$db="QFUckEO5eQ";
+
+$con = mysqli_connect($host, $username, $pass, $db);
+mysqli_select_db($con,$db) or die ('Cannot found database');
+
 
 /*
 $servername   = "localhost";
